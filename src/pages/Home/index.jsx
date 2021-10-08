ed0e96b51e73cc82329f82142ad39785533b40d9
daniel_fuzion <daniel_fuzion@hotmail.com>
2021-10-08 06:04:47 -0300

    add something to homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import { Card, CardActions, CardContent, Fab, IconButton, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import api from '../../services/api'
+import LoginIcon from '@mui/icons-material/Login';
+import { Link } from 'react-router-dom';
+
+
+
+const fabStyle = {
+  position: 'absolute',
+  bottom: '50vh',
+  right: '47vw',
+};
+
+
 
 export default function Home() {
+  const [filmes,setFilmes] = useState([])
+  const [categorias, setCategorias] = useState([])
+
+
+  async function getCategorias(){
+    const response = await api.get('/categoria')
+    if(response.data)
+      setCategorias(response.data)
+  }
+
+  async function getFilmes(){
+    const response = await api.get('/filme')
+    if(response.data)
+      setFilmes(response.data)
+  }
+
+useEffect(() => {
+  getCategorias()
+  getFilmes()
+}, [])
+
+
   return (
     <div>
-      HOMEPAGE
+       {filmes && filmes.map((filme) => (
+        <Card key={filme.id} sx={{ width:'15vw', maxHeight: '50%', marginLeft: '2vw', marginTop:'2vw', display: 'block', float:'left' }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {filme.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {categorias.find(categoria=> filme.categoria_id == categoria.id ).name}
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
+      <Fab color="primary" component={Link} to={"/login"} variant="extended" aria-label="add" sx={fabStyle}>
+        <LoginIcon/>
+        Login
+      </Fab>
     </div>
   )
 }
